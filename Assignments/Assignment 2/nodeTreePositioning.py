0031e72e21b665f61982e5cb4f37a0a2408b23bb
AsherIDE <asher.de.jong@student.uva.nl>
2024-02-21 14:32:44 +0100

    Made tree work for multiple networks and edges now have different colors. Also the visualizing is now performed with a simple function

--- Assignments/Assignment 2/nodeTreePositioning.py
+++ Assignments/Assignment 2/nodeTreePositioning.py
@@ -56,7 +56,7 @@ def organizeBfsOutput(input):
 # Determine node offsets for each node
 # Input --> levels output from organizeBfsOutput function
 # Ouput --> [{levels}, {node: {bottom_node: offset}}]
-def getOffsets(levels):
+def getOffsets(levels, distance=1):
     output = []
 
     for idx, level in enumerate(levels):
@@ -71,15 +71,24 @@ def getOffsets(levels):
 
         for top_node, bottom_nodes in level.items():
             max_offset = (len(bottom_nodes) / 2) - 1 #-1 because 1 max_offset = 0 idx
-            left_offset = 1
-            right_offset = -1
+            left_offset = distance
+            right_offset = -distance
 
             # determine the offset from the top node
             if idx > 0:
-                top_node_offset = elevated_dict[top_node]
 
-                left_offset = top_node_offset + 1
-                right_offset = top_node_offset - 1
+                # find top_node in current level if its not in elevated_dict
+                if top_node not in elevated_dict.keys():
+                    # assign a position to nodes connected to the same line manually
+                    for output_node in output_level.values():
+                        if top_node in output_node.keys():
+                            top_node_offset = output_node[top_node]
+                
+                else:
+                    top_node_offset = elevated_dict[top_node]
+
+                left_offset = top_node_offset + distance
+                right_offset = top_node_offset - distance
             
             # assign a position to every bottom node
             output_node_offsets = {}
@@ -88,12 +97,12 @@ def getOffsets(levels):
                 # assign left offset
                 if idx2 > max_offset:
                     output_node_offsets[bottom_node] = left_offset
-                    left_offset += 1
+                    left_offset += distance
 
                 # assign right offset
                 else:
                     output_node_offsets[bottom_node] = right_offset
-                    right_offset -= 1
+                    right_offset -= distance
 
             output_level[top_node] = output_node_offsets
 
@@ -103,7 +112,7 @@ def getOffsets(levels):
 
 
 # Get a dict with node coordinates
-def getCoordinates(offsets):
+def getCoordinates(offsets, distance=1):
     output = {}
 
     # get first node position
@@ -133,7 +142,7 @@ def getCoordinates(offsets):
                     # find new position for node
                     manual_offset = offset
                     while True:
-                        manual_offset += move
+                        manual_offset = manual_offset + (move * distance)
 
                         if manual_offset not in occupied_offsets:
                             output[node] = (manual_offset, -y)
@@ -143,53 +152,78 @@ def getCoordinates(offsets):
 
     return output
 
+
+# Input --> file, fontsize, circlesize
+def drawVisualization(FILE_NAME, fontsize, circlesize):
+
+    # Define the adjacency list
+    G = pydot.graph_from_dot_file(FILE_NAME)[0]
+
+    # Bfs tree
+    adjacency_list = CreateAdjacencyList(G.get_node_list(), G.get_edge_list())
+    top_node = getStartNode(adjacency_list)
+    bfs_list = bfs(removeAdjacencyListWeights(adjacency_list), top_node)
+
+    levels_list = organizeBfsOutput(bfs_list)
+    # for l in levels_list:
+    #     print(f"{l}\n")
+    offsets_list = getOffsets(levels_list, distance=10)
+
+    coordinates_dict = getCoordinates(offsets_list, distance=10)
+
+    # Draw nodes
+    # for node, position in coordinates_dict.items():
+    #     plt.scatter(position[0], position[1], color='#808080', zorder=2, s=350)
+    #     plt.text(position[0], position[1]-0.05, node, fontsize=12, ha='center', va='bottom', zorder=3, color='black')
+
+    for node, position in coordinates_dict.items():
+        plt.scatter(position[0], position[1], color='#808080', zorder=2, s=circlesize)
+        plt.text(position[0], position[1]-0.05, node, fontsize=fontsize, ha='center', va='bottom', zorder=3, color='black')
+
+    # Draw edges (only draw edges that come forth from the bfs output)
+    colors = ["#325A9B", "#EECA3B", "#FECB52", "#00CC96", "#636EFA", "#19D3F3", "#0D2A63", "#AB63FA", "#FF6692", "#BAB0AC", "#EF553B", "#6A76FC", "#E45756", "#479B55", "#72B7B2", "#1CBE4F", "#FF97FF", "#FF8000", "#B82E2E", "#FFA15A", "#54A24B", "#1C8356", "#FBE426", "#B6E880", "#AF0033", "#0099C6", "#325A9B", "#EECA3B", "#FECB52", "#00CC96", "#636EFA", "#19D3F3", "#0D2A63", "#AB63FA", "#FF6692", "#BAB0AC", "#EF553B", "#6A76FC", "#E45756", "#479B55", "#72B7B2", "#1CBE4F", "#FF97FF", "#FF8000", "#B82E2E", "#FFA15A", "#54A24B", "#1C8356", "#FBE426", "#B6E880", "#AF0033", "#0099C6"]
+    color_index = 0
+    for level in levels_list:
+        for top_node, bottom_nodes in level.items():
+            for node in bottom_nodes:
+                if node in coordinates_dict.keys() and top_node in coordinates_dict.keys():
+                    plt.plot([coordinates_dict[node][0], coordinates_dict[top_node][0]],
+                            [coordinates_dict[node][1], coordinates_dict[top_node][1]], color=colors[color_index], zorder=1, alpha=0.8)
+                
+            color_index += 1
+
+    # Draw edges (all edges according to adjacency list)
+    # for node, neighbors in adjacency_list.items():
+        
+    #     for neighbor in neighbors:
+
+    #         # prevent node not found from bvs output
+    #         if neighbor[0] not in coordinates_dict.keys() or node not in coordinates_dict.keys():
+    #             continue
+            
+    #         plt.plot([coordinates_dict[node][0], coordinates_dict[neighbor[0]][0]],
+    #                  [coordinates_dict[node][1], coordinates_dict[neighbor[0]][1]], color='black', zorder=1, alpha=0.5)
+
+
+    plt.title('Graph Visualization')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.xticks([])
+    plt.yticks([])
+
+    return plt.show()
+
+
 '''
 Below is where we actually start visualizing the tree
 '''
 
-# Define the adjacency list
-FILE_NAME = 'Networks/LesMiserables.dot'
-G = pydot.graph_from_dot_file(FILE_NAME)[0]
+# FILE_NAME = 'Networks/LesMiserables.dot'
+# drawVisualization(FILE_NAME, 12, 350)
 
-# Bfs tree
-adjacency_list = CreateAdjacencyList(G.get_node_list(), G.get_edge_list())
-top_node = getStartNode(adjacency_list)
-bfs_list = bfs(removeAdjacencyListWeights(adjacency_list), top_node)
+FILE_NAME = 'Networks/JazzNetwork.dot'
+drawVisualization(FILE_NAME, 6, 50)
 
-levels_list = organizeBfsOutput(bfs_list)
-offsets_list = getOffsets(levels_list)
+# FILE_NAME = 'Networks/LeagueNetwork.dot'
+# drawVisualization(FILE_NAME, 20, 500)
 
-coordinates_dict = getCoordinates(offsets_list)
-
-# Draw nodes
-for node, position in coordinates_dict.items():
-    plt.scatter(position[0], position[1], color='#808080', zorder=2, s=350)
-    plt.text(position[0], position[1]-0.05, node, fontsize=12, ha='center', va='bottom', zorder=3, color='black')
-
-# Draw edges (only draw edges that come forth from the bfs output)
-for level in levels_list:
-    for top_node, bottom_nodes in level.items():
-        for node in bottom_nodes:
-            plt.plot([coordinates_dict[node][0], coordinates_dict[top_node][0]],
-                    [coordinates_dict[node][1], coordinates_dict[top_node][1]], color='#808080', zorder=1, alpha=0.5)
-
-# Draw edges (all edges according to adjacency list)
-# for node, neighbors in adjacency_list.items():
-    
-#     for neighbor in neighbors:
-
-#         # prevent node not found from bvs output
-#         if neighbor[0] not in coordinates_dict.keys() or node not in coordinates_dict.keys():
-#             continue
-        
-#         plt.plot([coordinates_dict[node][0], coordinates_dict[neighbor[0]][0]],
-#                  [coordinates_dict[node][1], coordinates_dict[neighbor[0]][1]], color='black', zorder=1, alpha=0.5)
-
-
-plt.title('Graph Visualization')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.xticks([])
-plt.yticks([])
-
-plt.show()
