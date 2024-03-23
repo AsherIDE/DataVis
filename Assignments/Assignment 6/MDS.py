b8ef52294e943b35f156a3ebca6863d97026d04e
AsherIDE <asher.de.jong@student.uva.nl>
2024-03-23 23:56:26 +0100

    Fixed floyd_warshall algorithm and implemented MDS

---
+++ Assignments/Assignment 6/MDS.py
@@ -0,0 +1,38 @@
+import pydot
+import matplotlib.pyplot as plt
+
+from floyd_warshall import floyd_warshall
+from sklearn.manifold import MDS
+
+# testing 
+FILE_NAME = 'Networks/LesMiserables.dot'
+G = pydot.graph_from_dot_file(FILE_NAME)[0]
+
+X = floyd_warshall(G)
+
+embedding = MDS(n_components=2, normalized_stress='auto', dissimilarity='precomputed')
+
+X_transformed = embedding.fit_transform(X)
+
+# simple way to scatter
+# plt.scatter(*zip(*X_transformed))
+
+# scatter nice plot
+for i in range(len(X_transformed)):
+    x, y = X_transformed[i]
+
+    plt.scatter(x, y, color="#EECA3B", zorder=2, s=50)
+    plt.text(x, y, i, fontsize=5, ha='center', va='center', zorder=3, color='black')
+
+# draw edges
+for edge in G.get_edge_list():
+    source, sink = X_transformed[int(edge.get_source()) - 1], X_transformed[int(edge.get_destination()) - 1]
+
+    plt.plot([source[0], sink[0]],
+            [source[1], sink[1]], color="black", zorder=1, alpha=0.1)
+
+plt.title('Multidimensional scaling')
+plt.xticks([])
+plt.yticks([])
+
+plt.show()
---
+++ Assignments/Assignment 6/floyd_warshall.py
@@ -0,0 +1,34 @@
+import pydot
+import numpy as np
+
+def floyd_warshall(G):
+    # vertices
+    V = G.get_node_list()
+
+    dim = len(V)
+    D = np.ones((dim,dim)) * np.inf
+
+    # redefine existing edge weight values
+    for edge in G.get_edge_list():
+        # print(edge.get_source(), edge.get_destination(), edge.get_weight())
+        D[int(edge.get_source()) - 1, int(edge.get_destination()) - 1] = edge.get_weight()
+        D[int(edge.get_destination()) - 1, int(edge.get_source()) - 1] = edge.get_weight()
+
+    # set diagonals to 0
+    for i in range(len(V)):
+        D[i, i] = 0
+
+    # find paths with smaller weights
+    for k in range(dim): 
+        for i in range(dim): 
+            for j in range(dim):
+                if D[i, j] > D[i, k] + D[k, j]:
+                    D[i, j] = D[i, k] + D[k, j]
+
+    return D
+
+# testing 
+# FILE_NAME = 'Networks/LesMiserables.dot'
+# G = pydot.graph_from_dot_file(FILE_NAME)[0]
+
+# print(floyd_warshall(G))
